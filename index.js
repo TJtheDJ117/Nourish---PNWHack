42ea8c44c2d54592ff7bc37b929a43257696b1b2
TJtheDJ117 <tomjacob@live.com>
2023-06-03 15:19:14 -0700

    Downloaded Firebase

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("HELLO FROM INDEX.JS")
+import { initilizeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBU8jhw82rdDlTB37TIBdBoNrFWG5lTIjQ",
+    authDomain: "hackpnw-25ec0.firebaseapp.com",
+    projectId: "hackpnw-25ec0",
+    storageBucket: "hackpnw-25ec0.appspot.com",
+    messagingSenderId: "1048253409903",
+    appId: "1:1048253409903:web:4813b9a55d64a4fa0ebb9a"
+  };
+
+  initilizeApp(firebaseConfig)
